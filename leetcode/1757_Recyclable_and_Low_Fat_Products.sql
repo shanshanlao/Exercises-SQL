a0d6a49080022b33747b58fd2a3aa72adcab585f
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-01-31 23:18:16 -0500

    Create 1757_Recyclable_and_Low_Fat_Products.sql

---
+++ leetcode/1757_Recyclable_and_Low_Fat_Products.sql
@@ -0,0 +1,10 @@
+/*
+Write an SQL query to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+*/
+
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
