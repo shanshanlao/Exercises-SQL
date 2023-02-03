d20f708072fc8ca63e3d1f245efaec3cd1e3e015
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-02-02 19:42:22 -0500

    Update 1527_Patients_With_a_Condition.sql

--- leetcode/1527_Patients_With_a_Condition.sql
+++ leetcode/1527_Patients_With_a_Condition.sql
@@ -3,6 +3,12 @@ Write an SQL query to report the patient_id, patient_name and conditions of the 
 Type I Diabetes always starts with DIAB1 prefix.
 */
 
+-- Using LIKE
 SELECT *
 FROM Patients
 WHERE conditions LIKE '% DIAB1%' or conditions LIKE 'DIAB1%'
+
+-- Using Regex
+SELECT *
+FROM patients
+WHERE conditions regexp '^DIAB1|[:space:]DIAB1'
