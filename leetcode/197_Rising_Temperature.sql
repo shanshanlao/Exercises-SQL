ddbe5a28e2c03390a7d2a75ddc2466a54df8e865
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-02-04 21:02:22 -0500

    Corrected

--- leetcode/197_Rising_Temperature.sql
+++ leetcode/197_Rising_Temperature.sql
@@ -1,7 +1,7 @@
 /*
 Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
 */
-SELECT w2.id
+SELECT w1.id
 FROM Weather w1, Weather w2
-WHERE w1.recordDate = w2.recordDate-1
-AND w1.temperature < w2.temperature
+WHERE DATEDIFF(w1.recordDate , w2.recordDate) = 1
+AND w1.temperature > w2.temperature
