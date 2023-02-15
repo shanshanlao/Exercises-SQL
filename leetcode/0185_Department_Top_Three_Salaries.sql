32e60302aafbd6b5db9b4dac5ba40a6398822f4a
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-02-14 21:51:54 -0500

    Update 0185_Department_Top_Three_Salaries.sql

--- leetcode/0185_Department_Top_Three_Salaries.sql
+++ leetcode/0185_Department_Top_Three_Salaries.sql
@@ -5,13 +5,15 @@ Write an SQL query to find the employees who are high earners in each of the dep
 */
 
 WITH top_3_salary AS (
-    SELECT departmentId, salary, id,
-            DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) as 'salary_rank'
+    SELECT Employee.*,
+           DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) as 'salary_rank'
     FROM Employee
 )
 
-SELECT DISTINCT d.name as 'Department', e.name as 'Employee', e.salary as 'Salary' 
-FROM top_3_salary t, Employee e, Department d
-WHERE t.id = e.id AND e.departmentId = d.Id
+SELECT DISTINCT d.name as 'Department', 
+                t.name as 'Employee', 
+                t.salary as 'Salary' 
+FROM top_3_salary t, Department d
+WHERE t.departmentId = d.Id
 AND t.salary_rank IN (1,2,3)
 
