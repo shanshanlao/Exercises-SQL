adb1a38fc1f63a510cb9b600dd5ccbf9f7e7735a
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-03-22 17:31:28 -0400

    Create Tesla_Unfinished_Parts.sql

---
+++ DataLemur/Tesla_Unfinished_Parts.sql
@@ -0,0 +1,11 @@
+/*
+Tesla is investigating bottlenecks in their production, and they need your help to extract the relevant data. Write a query that determines which parts have begun the assembly process but are not yet finished.
+
+Assumptions:
+- Table parts_assembly contains all parts in production.
+- A part with no finish_date is an unfinished part.
+*/
+
+SELECT DISTINCT part 
+FROM parts_assembly
+WHERE finish_date IS NULL;
