6d80ea106a20dbdc7b996447ddebca216d324c7a
Shanshan Lao <65920056+shanshanlao@users.noreply.github.com>
2023-03-22 17:28:50 -0400

    Create Page_With_No_Likes.sql

---
+++ DataLemur/Page_With_No_Likes.sql
@@ -0,0 +1,10 @@
+/*
+Assume you are given the tables below about Facebook pages and page likes. 
+Write a query to return the page IDs of all the Facebook pages that don't have any likes. 
+The output should be in ascending order.
+*/
+
+SELECT page_id
+FROM pages 
+WHERE page_id NOT IN (SELECT page_id FROM page_likes)
+ORDER BY page_id;
